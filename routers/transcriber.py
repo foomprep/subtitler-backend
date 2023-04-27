119cdbf6abd486146bbc7dcfcd3035495a6b06ff
George Balch <george.balch@proton.me>
2023-04-26 22:38:00 -0700

    Parse timestamps and return parsed output

--- routers/transcriber.py
+++ routers/transcriber.py
@@ -11,6 +11,28 @@ router = APIRouter()
 from transcription import Transcriber
 transcriber = Transcriber()
 
+def generate_subtitles(transcription):
+    subtitles = {'start': [], 'end': [], 'text': []}
+    current_start = None
+    sentence = ''
+    matches = ['.', '?', '!']
+    for segment in transcription['segments']:
+        for word in segment['words']:
+            if not any([x in word['word'] for x in matches]):
+                if current_start is None:
+                    current_start = word['start']
+                sentence += word['word']
+            else:
+                if current_start is None:
+                    current_start = word['start']
+                sentence += word['word']
+                subtitles['start'].append(current_start)
+                subtitles['end'].append(word['end'])
+                subtitles['text'].append(sentence)
+                sentence = ''
+                current_start = None
+    return subtitles
+
 @router.post(
     "/transcribe",
 )
@@ -19,4 +41,5 @@ async def transcribe_file(file: UploadFile):
         content = await file.read()
         await out_file.write(content)
     transcription = transcriber.transcribe('tmp') 
-    return transcription
+    subtitles = generate_subtitles(transcription)
+    return subtitles
