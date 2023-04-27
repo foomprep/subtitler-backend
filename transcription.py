c6de8efbe0697dd031aaac65aefbde804b95148f
George Balch <george.balch@proton.me>
2023-04-26 22:42:48 -0700

    Switch to small whisper model

--- transcription.py
+++ transcription.py
@@ -2,7 +2,7 @@ import whisper
 
 class Transcriber:
     def __init__(self):
-        self.model = whisper.load_model('base')
+        self.model = whisper.load_model('small')
 
     def transcribe(self, media_path):
         result = self.model.transcribe(media_path, word_timestamps=True)
